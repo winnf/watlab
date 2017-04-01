87f96dc3072432947377397fd406128f69cccfd3
jamarshon <jasonl96@gmail.com>
2017-04-01 14:53:15 -0400

    Changing dashboard

--- public/javascripts/common/dashboard-ctrl.js
+++ public/javascripts/common/dashboard-ctrl.js
@@ -3,9 +3,9 @@ var app = angular.module('App');
 
 app.controller('DashboardCtrl', function($scope, $window, $location) {
 	$scope.dTiles = [
-		{name: 'Lab Equipment and Safety Compliance', url: '/esr'},
-		{name: 'Lab Projects and Staff', url: '/psr'},
-		{name: 'Lab Publications and Experiment', url: '/per'},
+		{name: 'Lab Equipment and Safety Compliance', url: '/esr', imgSrc: '/images/flask-outline.png'},
+		{name: 'Lab Projects and Staff', url: '/psr', imgSrc: '/images/id-card.png'},
+		{name: 'Lab Publications and Experiment', url: '/per', imgSrc: '/images/edit.png'},
 	];
 
 	$scope.navigateTo = function(dTile) {
