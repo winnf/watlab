12e17c3d638cea8ea60a621a88cfa3e5bd717e43
Winnie Fong <winnie_fon@hotmail.com>
2017-04-01 19:20:57 -0400

    edit function for notices

--- public/javascripts/projectsStaff/celltypes-constants.js
+++ public/javascripts/projectsStaff/celltypes-constants.js
@@ -1,7 +1,13 @@
+/*jslint node: true */
+/*global $, jQuery, alert, angular*/
 'use strict';
 var app = angular.module('App');
 
-app.constant('CELLTYPES',{
-  DATE: '/psr/view/cell-formatted-date.ejs',
-  PLAIN: '/psr/view/cell-plain.ejs'
+app.constant('CELLTYPES', {
+	CLICKABLE: '/psr/view/cell-clickable.ejs',
+	DELETE: '/psr/view/cell-delete.ejs',
+	DOWNLOAD: '/psr/view/cell-download.ejs',
+	DATE: '/psr/view/cell-formatted-date.ejs',
+	PLAIN: '/psr/view/cell-plain.ejs',
+	STATUS: '/psr/view/cell-status.ejs'
 });
